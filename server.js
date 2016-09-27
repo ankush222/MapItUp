910602c83f7663ae1f6dfa5858d150ee9615c97f
EC2 Default User <ec2-user@ip-172-31-38-181.us-west-2.compute.internal>
2016-09-27 05:20:06 +0000

    now connecting to database server

--- server.js
+++ server.js
@@ -16,18 +16,18 @@ var bodyParser = require('body-parser');
 app.use(express.static(path.join(__dirname, 'public')));
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
+  host: 'my-db-instance.clvztxhdj6v1.us-west-2.rds.amazonaws.com',
+  user: 'db_user',
   password: 'aadhar91',
   database: 'mapitup'
 })
 
 connection.connect();
 
-// connection.query('INSERT INTO users (firstName, lastName, email, password) VALUES (?, ?, ?, ?)', ['Ankush', 'Jain', 'jain97@purdue.edu', 'aadhar91'], function(err, rows, fields) {
-//   if (err)
-//     console.log("error in inserting!", err);
-// });
+connection.query('INSERT INTO users (firstName, lastName, email, password) VALUES (?, ?, ?, ?)', ['Ankush', 'Jain', 'jain97@purdue.edu', 'aadhar91'], function(err, rows, fields) {
+  if (err)
+   console.log("error in inserting!", err);
+});
 
 app.use(express.static('public'));
 app.get('/index', function (req, res) {
@@ -46,7 +46,7 @@ app.post('/test', function (req, res) {
     console.log(req.body.email);
 })
 
-var server = app.listen(8081, function () {
+var server = app.listen(3000, function () {
 
    var host = server.address().address
    var port = server.address().port
@@ -54,3 +54,4 @@ var server = app.listen(8081, function () {
    console.log("Example app listening at http://%s:%s", host, port)
 
 })
+
